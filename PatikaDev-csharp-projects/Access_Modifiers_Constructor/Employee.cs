36c9767180c4e8f8960cf97a9f31472ec647786f
İrem Çelikkanat <56557278+iremcelikkanat@users.noreply.github.com>
2023-10-19 23:31:20 +0300

    Constructor

---
+++ PatikaDev-csharp-projects/Access_Modifiers_Constructor/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers_Constructor
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public string LastName;
+        public string Department;
+
+
+        public Employee() { }
+
+        public Employee(string name, string lastName)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+
+        }
+        public Employee(string name, string lastName, int id, string department)
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Id = id;
+            this.Department = department;
+
+        }
+
+        public void EmployeeGet()
+        {
+
+            Console.WriteLine("Name of the employee: {0}", Name);
+            Console.WriteLine("Last Name of the employee: {0}", LastName);
+            Console.WriteLine("Id of the employee {0}:", Id);
+            Console.WriteLine("Departman of the employee: {0}", Department);
+
+        }
+
+    }
+}
---
+++ PatikaDev-csharp-projects/Access_Modifiers_Constructor/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Access_Modifiers_Constructor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Employee employee = new Employee("İrem","Çelikkanat");
+        employee.EmployeeGet();
+
+
+        Employee employee2 = new Employee("İrem", "Çelikkanat", 123456, "E-Commerce");
+        employee2.EmployeeGet();
+
+
+    }
+}
