b1fb74c98b6f1f892f1704da5e0f4be429958651
İrem Çelikkanat <56557278+iremcelikkanat@users.noreply.github.com>
2023-10-19 23:08:56 +0300

    Class field Instance

---
+++ PatikaDev-csharp-projects/PatikaDev-csharp-projects/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaDev_csharp_projects
+{
+    public class Employee
+    {
+        public int Id;
+        public string Name;
+        public string LastName;
+        public string Department;
+
+
+        public void EmployeeGet()
+        {
+
+        Console.WriteLine("Name of the employee: {0}", Name);
+        Console.WriteLine("Last Name of the employee: {0}", LastName);
+        Console.WriteLine("Id of the employee {0}:", Id);
+        Console.WriteLine("Departman of the employee: {0}", Department);
+
+        }
+
+    }
+
+}
+
+
+//1. Public : Her yerden erişilebilir.
+
+//2. Private : Sadece tanımlandığı sınıf içerisinden erişilebilir.
+
+//3. Internal : Sadece bulunduğu proje içerisinden erişilebilir
+
+//4. Protected : Sadece tanımlandığı sınıfta ya da o sınıfı miras alan sınıflardan erişilebilir.
---
+++ PatikaDev-csharp-projects/PatikaDev-csharp-projects/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PatikaDev_csharp_projects;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Employee employeeOne = new Employee();
+        employeeOne.Name = "İrem";
+        employeeOne.LastName = "Çelikkanat";
+        employeeOne.Id = 123456;
+        employeeOne.Department = "E-Commerce";
+
+        employeeOne.EmployeeGet();
+    }
+}
